54d9c31a774446b7b0825dbaf065398ed9832099
Jason Pereira <jasonpereira84@gmail.com>
2023-04-18 13:50:47 -0400

    Added extension methods

---
+++ Helpers.Web/Extensions/IHeaderDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace JasonPereira84.Helpers
+{
+    namespace Extensions
+    {
+        using Microsoft.AspNetCore.Http;
+
+        public static partial class Web
+        {
+            public static Dictionary<String, IEnumerable<String>> AsDictionary(this IHeaderDictionary headerDictionary)
+                => headerDictionary
+                    .ToDictionary(
+                        keySelector: kvp => kvp.Key,
+                        elementSelector: kvp => kvp.Value.AsStrings());
+
+        }
+    }
+}
---
+++ Helpers.Web/Extensions/StringValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace JasonPereira84.Helpers
+{
+    namespace Extensions
+    {
+        using Microsoft.Extensions.Primitives;
+
+        public static partial class Web
+        {
+            public static IEnumerable<String> AsStrings(this StringValues stringValues)
+                => stringValues
+                    .ToArray()//Do not change
+                    .SelectWhen(
+                        predicate: s => s.IsNotNullOrEmptyOrWhiteSpace(),
+                        selector: s => s.Trim());
+
+            public static String AsString(this StringValues stringValues)
+                => Misc.AsString(stringValues.AsStrings());
+
+            public static String AsString(this StringValues stringValues, String separator)
+                => Misc.AsString(stringValues.AsStrings(), separator);
+
+            public static String AsString(this StringValues stringValues, Char separator)
+                => Misc.AsString(stringValues.AsStrings(), separator);
+
+        }
+    }
+}
